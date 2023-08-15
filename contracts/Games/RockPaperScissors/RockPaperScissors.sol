d6ae50c3578e42663d707f661d2b3cfd096a142c
YoCash Ecosystem <133572784+yocashofficial@users.noreply.github.com>
2023-08-15 07:26:35 +0200

    Update RockPaperScissors.sol

--- contracts/Games/RockPaperScissors/RockPaperScissors.sol
+++ contracts/Games/RockPaperScissors/RockPaperScissors.sol
@@ -129,7 +129,7 @@ contract RockPaperScissors is Common {
     }
 
     function gameRefund() external nonReentrant {
-        require(tx.origin == msg.sender, "no contracts refunds allowed");
+        require(tx.origin == msg.sender, "no contracts refunds allowed");   // this is intended and will be compensated manually
         RockPaperScissorsGame storage game = rockPaperScissorsGames[msg.sender];
         if (game.requestID == 0) {
             revert NotAwaitingVRF();
