4643f51ab659c9d8f1dc7e75e60e93c1a0667e4c
YoCash Ecosystem <133572784+yocashofficial@users.noreply.github.com>
2023-08-15 07:26:11 +0200

    Update Dice.sol

--- contracts/Games/Dice/Dice.sol
+++ contracts/Games/Dice/Dice.sol
@@ -130,7 +130,7 @@ contract Dice is Common {
     }
 
     function gameRefund() external nonReentrant {
-        require(tx.origin == msg.sender, "no contracts refunds allowed");
+        require(tx.origin == msg.sender, "no contracts refunds allowed");  // this is intended and will be compensated manually
         DiceGame storage game = diceGames[msg.sender];
         if (game.requestID == 0) {
             revert NotAwaitingVRF();
